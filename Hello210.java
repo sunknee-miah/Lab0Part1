d319aaa2d6e32cb17fc69f583c7e87ea9e45d6a0
sunknee-miah <s11miah@siena.edu>
2026-09-14 09:41:48 -0400

    Added name and extra printout.

--- Hello210.java
+++ Hello210.java
@@ -2,12 +2,16 @@
 /**
  * CSIS 210 Setup Lab "Hello, World" program
  *
- * @author Prof. White
+ * @author Sunny Miah
  */
 public class Hello210 {
     
     public static void main(String args[]) {
         
         System.out.println("Hello CSIS 210 World!");
+        System.out.println("My hometown is Hudson NY");
+        System.out.println("Favorite Resturant is Wassabi");
+        System.out.println("Favorite item on the menu is Fuji Maki (Sushi)");
+        
     }
 }
